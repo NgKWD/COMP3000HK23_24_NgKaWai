0928c9f58470138a2365bac0243c1d1f3a9ee8b0
NgKWD <Ka.W.Ng-7@students.plymouth.ac.uk>
2024-08-18 23:41:24 -0400

    feat: update style for knowledge centre

--- src/components/KnowledgeCentre.js
+++ src/components/KnowledgeCentre.js
@@ -3,16 +3,24 @@ import React from 'react';
 const KnowledgeCentre = () => {
     return (
         <div>
-             {/* Banner Section */}
-             <div className="banner" style={{ backgroundImage: `url(assets/knowledgecentre.jpg)` }}>
+            {/* Banner Section */}
+            <div className="banner" style={{ backgroundImage: `url(assets/knowledgecentre.jpg)` }}>
                 <div className="banner-overlay"></div>
                 <h1 className="banner-text">Knowledge Centre</h1>
             </div>
-            <h2>Knowledge Centre</h2>
-            <p>Educational resources and articles.</p>
+            {/* Recent News Section */}
+            <div style={styles.mainContent}>
+                <h2>Knowledge Centre</h2>
+                <p>Waht is Information Security</p>
+                
+            </div>
         </div>
-        
+
     );
 };
-// const KnowledgeCentre 
+const styles = {
+    mainContent: {
+        padding: '0px 70px 70px 70px',
+    }
+};
 export default KnowledgeCentre;
